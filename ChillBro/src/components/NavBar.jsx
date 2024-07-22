b76b01d2861406297e927946c56a8e271d287de7
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-07-22 13:33:49 +0300

    setting up nav bar

---
+++ ChillBro/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+function NavBar() {
+  return;
+  <>
+    <nav className="navbar">
+      <div className="nav-barcontainer">
+        <Link to="/" className="navbar-logo"></Link>
+        Chill Bro
+      </div>
+    </nav>
+  </>;
+}
+
+export default NavBar;
