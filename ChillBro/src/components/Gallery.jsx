829edebea07aad1f30009aebbadd62a71137929c
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-07-24 14:26:07 +0300

    destinations added

--- ChillBro/src/components/Gallery.jsx
+++ ChillBro/src/components/Gallery.jsx
@@ -8,37 +8,37 @@ export function Gallery() {
       <div className="container">
         <div className="card">
           <h4>Hawaii</h4>
-          <img src=""></img>
+          <img src="./src/assets/images/hawaii.jpg"></img>
           <p></p>
         </div>
         <div className="card">
-          <h4>Hawaii</h4>
-          <img src=""></img>
+          <h4>Bali</h4>
+          <img src="./src/assets/images/bali.jpg"></img>
           <p></p>
         </div>
         <div className="card">
-          <h4>Hawaii</h4>
-          <img src=""></img>
+          <h4>Costa Rica</h4>
+          <img src="./src/assets/images/costarica.jpg"></img>
           <p></p>
         </div>
         <div className="card">
-          <h4>Hawaii</h4>
-          <img src=""></img>
+          <h4>Jamaica</h4>
+          <img src="./src/assets/images/jamaica.jpg"></img>
           <p></p>
         </div>
         <div className="card">
-          <h4>Hawaii</h4>
-          <img src=""></img>
+          <h4>Madeira</h4>
+          <img src="./src/assets/images/madeira.jpg"></img>
           <p></p>
         </div>
         <div className="card">
-          <h4>Hawaii</h4>
-          <img src=""></img>
+          <h4>Mallorca</h4>
+          <img src="./src/assets/images/mallorca.jpg"></img>
           <p></p>
         </div>
         <div className="card">
-          <h4>Hawaii</h4>
-          <img src=""></img>
+          <h4>Trinidad</h4>
+          <img src="./src/assets/images/trinidad.jpeg"></img>
           <p></p>
         </div>
       </div>
