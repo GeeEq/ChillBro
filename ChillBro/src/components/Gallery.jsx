80a1ad9cb0a053b51a9f254d9c1310db104e5823
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-07-28 14:16:29 +0300

    cards adjustment

--- ChillBro/src/components/Gallery.jsx
+++ ChillBro/src/components/Gallery.jsx
@@ -9,8 +9,9 @@ export function Gallery() {
         <div className="card">
           {/* <div className="cardsContainer"> */}
 
-          <h4>Hawaii</h4>
           <img src="./src/assets/images/hawaii.jpg"></img>
+          <h4>Hawaii</h4>
+
           <p>
             Discover the beauty, culture and adventures waiting for you on the
             Hawaiian Islands. Find Hawaii travel information and plan your
@@ -19,8 +20,9 @@ export function Gallery() {
           {/* </div> */}
         </div>
         <div className="card">
-          <h4>Bali</h4>
           <img src="./src/assets/images/bali.jpg"></img>
+          <h4>Bali</h4>
+
           <p>
             A magical blend of a colourful culture, friendly people, stunning
             nature, countless activities, tropical weather, culinary delights,
@@ -28,8 +30,9 @@ export function Gallery() {
           </p>
         </div>
         <div className="card">
-          <h4>Costa Rica</h4>
           <img src="./src/assets/images/costarica.jpg"></img>
+          <h4>Costa Rica</h4>
+
           <p>
             This beautiful country is known for its stunning beaches, lush
             rainforests, and incredible wildlife. Whether you are looking for
@@ -37,8 +40,9 @@ export function Gallery() {
           </p>
         </div>
         <div className="card">
-          <h4>Jamaica</h4>
           <img src="./src/assets/images/jamaica.jpg"></img>
+          <h4>Jamaica</h4>
+
           <p>
             Find fun things to do in Jamaica. Experience the vibrant culture,
             thrilling adventures, breathtaking beaches and irresistible flavors
@@ -46,8 +50,9 @@ export function Gallery() {
           </p>
         </div>
         <div className="card">
-          <h4>Madeira</h4>
           <img src="./src/assets/images/madeira.jpg"></img>
+          <h4>Madeira</h4>
+
           <p>
             Breathtaking natural settings, crystal-clear waters, centuries-old
             traditions, genuine flavours and a great, great desire to welcome
@@ -56,8 +61,9 @@ export function Gallery() {
           </p>
         </div>
         <div className="card">
-          <h4>Mallorca</h4>
           <img src="./src/assets/images/mallorca.jpg"></img>
+          <h4>Mallorca</h4>
+
           <p>
             Imagine an island with turquoise blue waters, secret coves, soft
             golden sands, lush green foliage, rugged mountains, picturesque
@@ -67,8 +73,9 @@ export function Gallery() {
           </p>
         </div>
         <div className="card">
-          <h4>Trinidad</h4>
           <img src="./src/assets/images/trinidad.jpeg"></img>
+          <h4>Trinidad</h4>
+
           <p>
             Trinidad and Tobago is a beautiful country in the Caribbean, located
             just off the coast of Venezuela. Known for its vibrant culture,
