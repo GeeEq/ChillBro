bd97a74895e0e2ef386f4500bf90795ab193f54f
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-07-30 14:25:58 +0300

    styling contacts form

--- ChillBro/src/components/Contacts.jsx
+++ ChillBro/src/components/Contacts.jsx
@@ -6,9 +6,35 @@ export function Contacts() {
     <section className="contactsSec">
       <form>
         <h2>Contacts</h2>
-        <div className="input-box"></div>
-        <label>Full Name</label>
-        <input type="text" className="field" placeholder="Enter Your Name" />
+        <div className="input-box">
+          <label>Full Name</label>
+          <input
+            type="text"
+            className="field"
+            placeholder="Enter Your Name"
+            required
+          />
+        </div>
+        <div className="input-box">
+          <label>Email Address</label>
+          <input
+            type="text"
+            className="field"
+            placeholder="Enter Your Email"
+            required
+          />
+        </div>
+        <div className="input-box">
+          <label>Your Message</label>
+          <textarea
+            name=""
+            id=""
+            className="field mess"
+            placeholder="Enter Your Message"
+            required
+          />
+        </div>
+        <button type="submit">Send Message</button>
       </form>
     </section>
   );
