21133f0e7d72fb26d2e42b4aab6cf605de8e983e
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-07-30 15:00:44 +0300

    implementing web3forms

--- ChillBro/src/components/Contacts.jsx
+++ ChillBro/src/components/Contacts.jsx
@@ -2,16 +2,39 @@ import React from "react";
 import "./Contacts.css";
 
 export function Contacts() {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "87e9e587-6510-4239-8656-569d46a79e68");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+    }
+  };
   return (
     <section className="contactsSec">
       <form>
         <h2>Contacts</h2>
-        <div className="input-box">
+        <div className="input-box fullName">
           <label>Full Name</label>
           <input
             type="text"
             className="field"
             placeholder="Enter Your Name"
+            name="name"
             required
           />
         </div>
@@ -21,20 +44,23 @@ export function Contacts() {
             type="text"
             className="field"
             placeholder="Enter Your Email"
+            name="email"
             required
           />
         </div>
         <div className="input-box">
           <label>Your Message</label>
           <textarea
-            name=""
+            name="message"
             id=""
             className="field mess"
             placeholder="Enter Your Message"
             required
           />
         </div>
-        <button type="submit">Send Message</button>
+        <button type="submit" onClick={onSubmit}>
+          Send Message
+        </button>
       </form>
     </section>
   );
