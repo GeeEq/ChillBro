9c5173677df86bc37799dfa5d7c2379cd85da846
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-07-30 14:10:34 +0300

    contact form in progress

--- ChillBro/src/components/Contacts.jsx
+++ ChillBro/src/components/Contacts.jsx
@@ -3,8 +3,13 @@ import "./Contacts.css";
 
 export function Contacts() {
   return (
-    <div className="contactsSec">
-      <h2>Contacts</h2>
-    </div>
+    <section className="contactsSec">
+      <form>
+        <h2>Contacts</h2>
+        <div className="input-box"></div>
+        <label>Full Name</label>
+        <input type="text" className="field" placeholder="Enter Your Name" />
+      </form>
+    </section>
   );
 }
